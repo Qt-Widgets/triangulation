6b9587a86e2e13aa4b2452525f670fae3f09deeb
Никита Ваганов <nikita.vaganov@vniira.spb.su>
2013-06-03 19:07:54 +0400

    добавлено преобразование координат с помощью метода mapToScene()

--- demo/MyGraphicsView.cpp
+++ demo/MyGraphicsView.cpp
@@ -5,13 +5,7 @@
 #include "MyGraphicsView.h"
 
 void MyGraphicsView::mouseDoubleClickEvent (QMouseEvent* event) {
-    QTextStream(stdout) << "event->pos() = {" << event->pos().x() << ", " << event->pos().y() << "}\n";
-    QTextStream(stdout) << "event->posF() = {" << event->posF().x() << ", " << event->posF().y() << "}\n";
-    QTextStream(stdout) << "event->globalPos() = {" << event->globalPos().x() << ", " << event->globalPos().y() << "}\n";
-    QPointF vertex = event->posF();
-//  QPointF vertex = event->globalPos();
+    QPointF vertex = mapToScene(event->pos());
     m_vertex.append(vertex);
-    QGraphicsEllipseItem* ellipse = scene()->addEllipse(vertex.x() - 3, vertex.y() - 3, 5, 5);
-//  QGraphicsEllipseItem* ellipse = scene()->addEllipse(event->x() - 3, event->y() - 3, 5, 5);
-    QTextStream(stdout) << "ellipse->rect() = {" << ellipse->rect().x() << ", " << ellipse->rect().y() << "}\n";
+    scene()->addEllipse(vertex.x() - 3, vertex.y() - 3, 5, 5);
 }
